24363e27c14627e5700138324d7dc556b30cd921
KaasLarsen <vallelarsen@yahoo.dk>
2025-11-07 17:06:22 +0100

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+export const config = { runtime: "edge" };
+
+export default async function handler() {
+  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
+    status: 200
+  });
+}
