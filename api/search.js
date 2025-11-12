6bc383b35be4ebeec2ddfe2273b66456a52ae28f
KaasLarsen <vallelarsen@yahoo.dk>
2025-11-12 13:32:06 +0100

    Update search.js

--- api/search.js
+++ api/search.js
@@ -5,11 +5,20 @@ export default async function handler(req, res) {
     const qRaw = (req.query.q || "").toString().trim();
     const budgetMax = req.query.max ? parseInt(req.query.max, 10) : null;
 
-    const FEEDS = [
-      { merchant: "Mere om Vin",  url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=87611&feedid=2182" },
-      { merchant: "Winther Vin",  url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=76708&feedid=1766" },
-      { merchant: "Vinea",        url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=111911&feedid=3767" }
-    ];
+const FEEDS = [
+  { merchant: "Mere om Vin",        url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=87611&feedid=2182" },
+  { merchant: "Winther Vin",        url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=76708&feedid=1766" },
+  { merchant: "Vinea",              url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=111911&feedid=3767" },
+  { merchant: "Barlife",            url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=46609&feedid=651" },
+  { merchant: "DH Wines",           url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=108173&feedid=3461" },
+  { merchant: "D’Wine",             url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=92927&feedid=2455" },
+  { merchant: "Gourmetshoppen",     url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=80950&feedid=1925" },
+  { merchant: "Johnsen Vine",       url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=114732&feedid=4073" },
+  { merchant: "SPS Wine",           url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=112662&feedid=3860" },
+  { merchant: "Westjysk Smag",      url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=91648&feedid=2398" },
+  { merchant: "Winesommelier",      url: "https://www.partner-ads.com/dk/feed_udlaes.php?partnerid=50537&bannerid=114219&feedid=4021" }
+];
+
 
     const headers = {
       "user-agent": UA,
